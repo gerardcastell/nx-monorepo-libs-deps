b0daf446cc862305fb14c2ad42f493c9cdb152f9
Gerard Cleeverea <gerard.castell@cleverea.com>
2021-07-20 14:53:01 +0200

    [ADD] initial config

--- libs/core/environments/src/lib/environment.ts
+++ libs/core/environments/src/lib/environment.ts
@@ -3,29 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  releaseTagVersion: 'dev',
-  runnerEnvironment: 'dev',
-  production: false,
-  gtag: 'UA-170465866-1',
-  name: 'Local',
-  api: 'https://api-staging.cleverea.com/api/flights',
-  apiUser: 'https://api-staging.cleverea.com/api/users',
-  apiMoto: 'https://api-staging.cleverea.com/api/moto',
-  apiPmv: 'https://api-staging.cleverea.com/api/pmv',
-  apiRenters: 'https://api-staging.cleverea.com/api/renters',
-  apiPayments: 'https://api-staging.cleverea.com/api/payments',
-  urlGateway: 'https://api-staging.cleverea.com/gateway',
-  //urlGateway: 'http://localhost:8080/http://localhost:3000/gateway',
-  apiClientId: 'XotEa9Ny9T4cFXvX2X0EZGjA',
-  apiSecretKey: 'mc1FEjQsm1INTmdPOTPhVS75EUtc0awIPAoqPskkrVZF1H9w',
-  s3BaseURLImages: 'https://images.cleverea.com',
-  s3BaseURLDocs:
-    'https://legal-public-docs-cleverea.s3-eu-west-1.amazonaws.com',
-  s3BaseURLPublicImg:
-    'https://public-images-web-cleverea.s3-eu-west-1.amazonaws.com',
-  apiKeyGateway: 'ca03na188ame03u1d78620de67282882a84',
-  landingBaseUrl: 'https://staging.cleverea.com',
-  country: 'ES',
+  api: 'api',
 };
 
 /*
