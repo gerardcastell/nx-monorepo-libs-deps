492a422aaf4c8d6df525a338da3dd967bf181e75
Gerard Cleeverea <gerard.castell@cleverea.com>
2021-09-09 09:57:29 +0200

    [ADD] error when building module with exported classes

---
+++ libs/core/buildable/README.md
@@ -0,0 +1,7 @@
+# core-buildable
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test core-buildable` to execute the unit tests.
---
+++ libs/core/buildable/src/lib/component-a/component-a.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lab-component-a',
+  templateUrl: './component-a.component.html',
+  styleUrls: ['./component-a.component.scss'],
+})
+export class ComponentAComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
